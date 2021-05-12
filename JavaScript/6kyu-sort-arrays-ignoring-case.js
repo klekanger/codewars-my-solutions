9546a1a1338f46e1a863ac4cd7d4079404640866
Kurt Lekanger <kurt@lekanger.no>
2021-05-12 19:07:30 +0200

    Solved another one... easy...

--- JavaScript/6kyu-sort-arrays-ignoring-case.js
+++ JavaScript/6kyu-sort-arrays-ignoring-case.js
@@ -1,3 +1,10 @@
+// Codewars
+// Kata: Sort the given array of strings in alphabetical order, case insensitive.
+// https://www.codewars.com/kata/51f41fe7e8f176e70d0002b9
+//
+// Solution by Kurt Lekanger
+// https://www.codewars.com/users/klekanger
+
 sortme = function (names) {
   return names.sort((a, b) => (a.toLowerCase() < b.toLowerCase() ? -1 : 1));
 };
